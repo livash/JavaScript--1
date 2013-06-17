77328d2efd664e16172a159ab6bdb6a0decc8552
= <=>
2013-06-17 16:07:14 -0700

    TicTacToe#isGameOver().

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,4 +1,5 @@
 // Tic-Tac-Toe game
+_ = require('underscore');
 
 function TicTacToe(mark1, mark2) {
   this.mark1 = mark1;
@@ -6,6 +7,18 @@ function TicTacToe(mark1, mark2) {
   this.emptyMark = '_';
   this.board = [[], [], []];
   this.initializeBoard(this.emptyMark);
+  this.winCombs = {
+    1: [0, 1, 2],
+    2: [3, 4, 5],
+    3: [6, 7, 8],
+
+    4: [0, 3, 6],
+    5: [1, 4, 7],
+    6: [2, 5, 8],
+
+    7: [0, 4, 8],
+    8: [2, 4, 6]
+  };
 };
 
 TicTacToe.prototype.initializeBoard = function(emptyMark) {
@@ -18,19 +31,16 @@ TicTacToe.prototype.initializeBoard = function(emptyMark) {
 
 TicTacToe.prototype.makeMove = function(row, col, mark) {
   if (this.isPositionEmpty(row, col)) {
-    this.board[row][col] = mark;
+    return this.board[row][col] = mark;
   } else {
     console.log("Move [" + row + ", " + col + "] is invalid");
+    return false;
   }
 }
 
 TicTacToe.prototype.isPositionEmpty = function(row, col) {
   var mark = this.board[row][col];
-  if (mark === this.emptyMark) {
-    return true;
-  } else {
-    return false;
-  }
+  return (mark === this.emptyMark);
 }
 
 TicTacToe.prototype.print = function() {
@@ -41,12 +51,28 @@ TicTacToe.prototype.print = function() {
 }
 
 TicTacToe.prototype.isGameOver = function() {
-  var winCombinations = this.winCombinations();
-
+  var markOnePositions = this.getMarkPositions(this.mark1);
+  var markTwoPositions = this.getMarkPositions(this.mark2);
+  for (var seq in this.winCombs) {
+    if (_.isEqual(_.intersection(markOnePositions, this.winCombs[seq]), this.winCombs[seq])) {
+      return true;
+    } else if (_.isEqual(_.intersection(markTwoPositions, this.winCombs[seq]), this.winCombs[seq])) {
+      return true;
+    }
+  }
+  return false;
 }
 
-TicTacToe.prototype.winCombinations = function() {
-
+TicTacToe.prototype.getMarkPositions = function(mark) {
+  var result = [];
+  for(var ii = 0; ii < this.board.length; ii++) {
+    for(var jj = 0; jj < this.board.length; jj ++) {
+      if (this.board[ii][jj] === mark) {
+        result.push(jj + ii * 3); //convert from [ii][jj] to single index
+      }
+    }
+  }
+  return result;
 }
 
 // testing here
@@ -54,6 +80,10 @@ var game = new TicTacToe('x', 'o');
 //console.log(game.board);
 game.print();
 game.makeMove(0, 0, game.mark1);
+console.log(game.isGameOver());
+game.makeMove(1, 1, game.mark1);
 game.print();
-// game.makeMove(0, 0, game.mark2);
-// game.print();
+console.log(game.isGameOver());
+game.makeMove(2, 2, game.mark1);
+game.print();
+console.log(game.isGameOver());
