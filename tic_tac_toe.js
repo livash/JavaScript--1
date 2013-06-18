f4ae8c44c7cce93e9df8ab7f83924ded8f6cfc29
= <=>
2013-06-17 17:15:06 -0700

    README + use _.each in tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -5,33 +5,34 @@ function TicTacToe(mark1, mark2) {
   this.mark1 = mark1;
   this.mark2 = mark2;
   this.emptyMark = '_';
-  this.board = [[], [], []];
+  this.board = [[null, null, null], [null, null, null], [null, null, null]];
   this.initializeBoard(this.emptyMark);
-  this.winCombs = {
-    1: [0, 1, 2],
-    2: [3, 4, 5],
-    3: [6, 7, 8],
+  this.winCombs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
 
-    4: [0, 3, 6],
-    5: [1, 4, 7],
-    6: [2, 5, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
 
-    7: [0, 4, 8],
-    8: [2, 4, 6]
-  };
+    [0, 4, 8],
+    [2, 4, 6] ]
 };
 
 TicTacToe.prototype.initializeBoard = function(emptyMark) {
-  for(var ii = 0; ii < this.board.length; ii++) {
-    for (var jj = 0; jj < this.board.length; jj++) {
-      this.board[ii][jj] = emptyMark;
-    }
-  }
+  var that = this;
+  _.each(that.board, function(row, rowIdx) {
+    _.each(row, function(col, colIdx) {
+      that.board[rowIdx][colIdx] = emptyMark;
+    });
+  });
 }
 
 TicTacToe.prototype.makeMove = function(row, col, mark) {
   if (this.isPositionEmpty(row, col)) {
-    return this.board[row][col] = mark;
+    this.board[row][col] = mark;
+    return true;
   } else {
     console.log("Move [" + row + ", " + col + "] is invalid");
     return false;
@@ -53,14 +54,16 @@ TicTacToe.prototype.print = function() {
 TicTacToe.prototype.isGameOver = function() {
   var markOnePositions = this.getMarkPositions(this.mark1);
   var markTwoPositions = this.getMarkPositions(this.mark2);
-  for (var seq in this.winCombs) {
-    if (_.isEqual(_.intersection(markOnePositions, this.winCombs[seq]), this.winCombs[seq])) {
-      return true;
-    } else if (_.isEqual(_.intersection(markTwoPositions, this.winCombs[seq]), this.winCombs[seq])) {
-      return true;
+  var result = false;
+  _.each(this.winCombs, function(comb) {
+    if (_.isEqual(_.intersection(markOnePositions, comb), comb)) {
+      result = true;
+    } else if (_.isEqual(_.intersection(markTwoPositions, comb), comb)) {
+      result = true;
     }
-  }
-  return false;
+  });
+
+  return result;
 }
 
 TicTacToe.prototype.getMarkPositions = function(mark) {
@@ -78,12 +81,13 @@ TicTacToe.prototype.getMarkPositions = function(mark) {
 // testing here
 var game = new TicTacToe('x', 'o');
 //console.log(game.board);
+//console.log(game.winCombs);
 game.print();
 game.makeMove(0, 0, game.mark1);
-console.log(game.isGameOver());
+//console.log(game.isGameOver());
 game.makeMove(1, 1, game.mark1);
 game.print();
-console.log(game.isGameOver());
+//console.log(game.isGameOver());
 game.makeMove(2, 2, game.mark1);
 game.print();
 console.log(game.isGameOver());
